6f83d2f15cd3768f3ee497e9c0894f87263d7eac
Julipu3 <julia.pim55@gmail.com>
2023-08-23 22:16:19 +0300

    changes

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,46 @@
+import SimpleLightbox from "simplelightbox"; 
+import 'simplelightbox/dist/simple-lightbox.min.css'; 
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+const gallery = document.querySelector(".gallery"); 
+ 
+gallery.addEventListener("click", handleClick); 
+ 
+function createMarkup() { 
+ 
+    const element = galleryItems.map(item => ` 
+    <li class="gallery__item">  
+  <a class="gallery__link" href="${item.original}"> 
+    <img 
+      class="gallery__image" 
+      src="${item.preview}" 
+      data-source="${item.original}" 
+      alt="${item.description}" 
+      target="_parent" 
+    /> 
+  </a> 
+</li> 
+    `).join(""); 
+ 
+    gallery.innerHTML = element; 
+}; 
+ 
+createMarkup() 
+ 
+function handleClick(event) { 
+    event.preventDefault(); 
+ 
+    if (event.target.nodeName === "IMG") { 
+        const lightbox = new SimpleLightbox('.gallery a', { 
+            captionsData: 'alt', 
+            captionDelay: 250, 
+        }); 
+         
+        lightbox.open(); 
+    } 
+} 
+ 
+gallery.addEventListener("click", handleClick); 
+ 
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+const keyStorage = "feedback-form-state";
+let currentData = {
+  email: "",
+  message: "",
+};
+
+form.addEventListener('input', throttle(onInput, 500));
+
+const currentLocalData = JSON.parse(localStorage.getItem(keyStorage));
+
+if (currentLocalData) {
+  form.email.value = currentLocalData.email;
+  form.message.value = currentLocalData.message;
+  currentData = currentLocalData;
+}
+
+function onInput() {
+  currentData = {
+    email: form.email.value,
+    message: form.message.value,
+  };
+
+  localStorage.setItem(keyStorage, JSON.stringify(currentData));
+}
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  console.log(currentData);
+  localStorage.removeItem(keyStorage);
+  form.reset();
+}
